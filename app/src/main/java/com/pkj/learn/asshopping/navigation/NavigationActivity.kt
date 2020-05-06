b2b434c736dd166abbeddd48d86327c486a01ff3
pankaj.jangid@decurtis.com <pankaj.jangid@decurtis.com>
2020-05-06 21:06:32 +0530

    - Added menu for navigation

--- app/src/main/java/com/pkj/learn/asshopping/navigation/NavigationActivity.kt
+++ app/src/main/java/com/pkj/learn/asshopping/navigation/NavigationActivity.kt
@@ -1,12 +1,21 @@
 package com.pkj.learn.asshopping.navigation
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.pkj.learn.asshopping.R
+import com.pkj.learn.asshopping.products.ProductsFragmentDirections
 
 class NavigationActivity : AppCompatActivity() {
 
@@ -22,8 +31,19 @@ class NavigationActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.fragme nt_holder, ProductsFragment.newInstance())
-//            .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 }
